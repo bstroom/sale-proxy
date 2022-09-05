2f19f21e19c2b49cceebd0702b83d970a1109415
Duc Lux <nguyenhuuduc@jv-it.com.vn>
2022-09-05 12:00:55 +0700

    fix

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -2,15 +2,15 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <link rel="icon" href="/favicon.ico" />
+    <link rel="icon" href="{{asset('/favicon.ico')}}" />
     <meta name="viewport" content="width=device-width,initial-scale=1" />
     <meta name="theme-color" content="#000000" />
     <meta name="description" content="Web site created using create-react-app" />
-    <link rel="apple-touch-icon" href="build/logo192.png" />
-    <link rel="manifest" href="build/manifest.json" />
+    <link rel="apple-touch-icon" href="{{asset('build/logo192.png')}}" />
+    <link rel="manifest" href="{{asset('build/manifest.json')}}" />
     <title>Proxy Quốc tế</title>
-    <script defer="defer" src="build/static/js/main.143ff600.js"></script>
-    <link href="build/static/css/main.9a0c1401.css" rel="stylesheet">
+    <script defer="defer" src="{{asset('build/static/js/main.143ff600.js')}}"></script>
+    <link href="{{asset('build/static/css/main.9a0c1401.css')}}" rel="stylesheet">
 </head>
 <body><noscript>You need to enable JavaScript to run this app.</noscript>
 <div id="root"></div>
