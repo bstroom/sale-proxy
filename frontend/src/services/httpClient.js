76abac7e01cde566fa1a1067ab11bb83238304ba
Duc Lux <nguyenhuuduc@jv-it.com.vn>
2022-10-14 23:09:59 +0700

    change url

--- frontend/src/services/httpClient.js
+++ frontend/src/services/httpClient.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {TOKEN_KEY} from "../common/contanst";
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https:/1proxy.net/:8000/api',
 });
 
 httpClient.interceptors.response.use((res) => {
